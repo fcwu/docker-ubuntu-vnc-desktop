76bb5839bef46b0f22440ac9a97d379e2e99f2bd
Colin <colinhuang@qnap.com>
2015-03-03 23:21:09 +0800

    Fix duplication action in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir /var/run/sshd
+mkdir -p /var/run/sshd
 
 # create an ubuntu user
-PASS=`pwgen -c -n -1 10`
+# PASS=`pwgen -c -n -1 10`
 PASS=ubuntu
-echo "User: ubuntu Pass: $PASS"
-useradd --create-home --shell /bin/bash --user-group --groups adm,sudo ubuntu
+# echo "Username: ubuntu Password: $PASS"
+id -u ubuntu &>/dev/null || useradd --create-home --shell /bin/bash --user-group --groups adm,sudo ubuntu
 echo "ubuntu:$PASS" | chpasswd
 
 /usr/bin/supervisord -c /supervisord.conf

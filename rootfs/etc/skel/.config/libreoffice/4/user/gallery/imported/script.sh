a44bf9351395aa78ba8b1e37031b2ab1dee80878
Devin Dice <bpapilot@gmail.com>
2023-01-11 14:46:10 -0500

    Fixing libreoffice

--- rootfs/etc/skel/.config/libreoffice/4/user/gallery/imported/script.sh
+++
@@ -1,7 +0,0 @@
-if $(echo $1 | grep svg > /dev/null); then
-       	path=$(dirname "$1")
-	file=$(basename "$1")
-	#echo $1 | tee -a log.txt
-	newName=$(echo "$file" | sed -e 's/_48//g' -e 's/_Light//g' -e 's/Res_//g' -e 's/Arch_//g' -e 's/AWS-//g' -e 's/Amazon-//g' -e 's/ //g' -e 's/ArchCategory//g')
-	mv "${path}/${file}" "${path}/${newName}"
-fi

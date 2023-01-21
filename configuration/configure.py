545a55b0778024be65a27b640850c5aa3e3f873e
Devin Dice <bpapilot@gmail.com>
2023-01-21 00:23:51 -0500

    No desktop option

--- configuration/configure.py
+++ configuration/configure.py
@@ -4,6 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--desktop", help="Accepts 'enabled' or 'disabled'")
+parser.add_argument("--ubuntu", help="Accepts '20' or '22'")
 args = parser.parse_args()
 
 # Default Nginx
@@ -25,6 +26,19 @@ filename = f"../rootfs/etc/supervisor/conf.d/supervisord.conf"
 
 content = template.render(
     desktop=args.desktop,
+)
+with open(filename, mode="w", encoding="utf-8") as message:
+    message.write(content)
+    print(f"... wrote {filename}")
+    
+    
+# Supervisor Conf
+environment = Environment(loader=FileSystemLoader("./"))
+template = environment.get_template("Dockerfile.j2")
+filename = f"../Dockerfile"
+
+content = template.render(
+    version=args.ubuntu,
 )
 with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
